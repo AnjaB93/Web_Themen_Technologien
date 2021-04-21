19336bb9efb52444941659b5f5827a0affb76b38
AnjaB93 <buan1022@hs-karlsruhe.de>
2021-04-21 08:48:47 +0200

    add Javascript

--- javascript.js
+++ javascript.js
@@ -1,5 +1,24 @@
-var myContainer = document.querySelectorAll("a .container-box");
+var myContainer = document.querySelectorAll("a[href='']");
+for (var i = 0; i < myContainer.length; i++) {
+  myContainer[i].style.opacity = 0.5;
+  myContainer[i].style.pointerEvents = "none";
+}
+
+/*
+var myContainer = document.querySelectorAll("a[href$='#']");
 for (var i = 0; i < myContainer.length; i++) {
   console.log(myContainer[i] + "hallo");
   myContainer[i].style.opacity = 0.5;
-}
+  
+}*/
+
+/*
+var allElements = document.querySelectorAll("a");
+for (var i = 0; i < allElements.length; i++) {
+  var myContainer = allElements[i];
+  if (myContainer.getAttribute("[href$='.html']")) {
+    myContainer.style.opacity = 1;
+  } else {
+    myContainer.style.opacity = 0.5;
+  }
+}*/
